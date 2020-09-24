f1d97ac4f438e6b1930070589afc107c6ba527fe
Jaslin2402 <71544042+Jaslin2402@users.noreply.github.com>
2020-09-24 17:54:00 +0530

    removed keys

--- aws/6_remote_state/remote-state.tf
+++ aws/6_remote_state/remote-state.tf
@@ -3,8 +3,8 @@ terraform {
     bucket  = "jaslin-s3"
     key  = "terraform/state"
     region = "us-east-1"
-#   access_key = "XXXXXXXXXXXXXXXXXXXXXX"
-#   secret_key = "XXXXXXXXXXXXXXXXXX"
+#   access_key = ""
+#   secret_key = ""
   }
 }
 
